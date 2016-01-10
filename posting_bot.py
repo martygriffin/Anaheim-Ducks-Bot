e809c05748b3855b3c6be5001d03531025ed270e
martygriffin <marty.griffin@me.com>
2016-01-10 13:14:01 -0800

    Anaheim Ducks Bot Initial Commit
    -support posting pregame threads for games

---
+++ posting_bot.py
@@ -0,0 +1,50 @@
+import praw
+import OAuth2Util
+from dateutil.parser import parse
+from datetime import timedelta, datetime
+import time
+
+import csv
+
+def convertToDict(line):
+	gameDict = {}
+	date = parse(line[0] + " "+line [1])
+	gameDict["startDate"] = date
+	gameDict["startTime"] = line[1]
+	gameDict["teams"] = line[2]
+	gameDict["location"] = line[3]
+	gameDict["description"] = line[4]
+	return gameDict
+def makePregamePost(game):
+	print("connecting.....")
+	user_agent = "Anaheim Ducks GameDay 1.0 by /u/dahooddawg"
+	r = praw.Reddit(user_agent=user_agent)
+	o = OAuth2Util.OAuth2Util(r)
+	o.refresh(force=True)
+	r.submit('subreddit name goes here', game["startDate"].date().strftime("%B %d, %Y") +" - "+game["teams"], text=game["startTime"]+" PST - "+game["location"]+" - "+game["description"])
+	print("posted for "+game["teams"])
+
+
+reader = csv.reader(open('ducks.csv','rb'))
+already_done = []
+
+while(True):
+
+	for line in reader:
+			#convert game data to Dict
+        	game = convertToDict(line)
+        	#check if there is a game today
+    		if(game["startDate"].date() == datetime.today().date()):
+    			postTime = game["startDate"] + timedelta(hours=-12)
+    			#check if it is 12 hours before game time
+    			if(postTime < datetime.today() and game["startDate"].date() not in already_done):
+    				#make pregram thread post
+    				makePregamePost(game)
+    				#make that this game was posted so we do not duplicate any work
+    				already_done.append(game["startDate"].date())
+    				break
+    #sleep for 30 mins
+    sleep(1800)
+
+
+
