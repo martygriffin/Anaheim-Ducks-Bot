6cfd243bc55dc4a45b7bf0af7ff88255e36f5417
martygriffin <marty.griffin@me.com>
2016-02-15 23:21:10 -0800

    Added support for posting game threads
    game threads are now posted 30 minutes before the start of each game

--- posting_bot.py
+++ posting_bot.py
@@ -4,58 +4,82 @@ from dateutil.parser import parse
 from datetime import timedelta, datetime
 import time
 import csv
+import urllib3
+import certifi
 import sys
+import gc
+import os
 
-print ("Duck Bot Start Up")
+user_agent = "Anaheim Ducks GameDay 1.0 by /u/dahooddawg"
+subreddit = "insert subreddit name here"
+
 
 def convertToDict(line):
-	gameDict = {}
-	date = parse(line[0] + " "+line [1])
-	gameDict["startDate"] = date
-	gameDict["startTime"] = line[1]
-	gameDict["teams"] = line[2]
-	gameDict["location"] = line[3]
-	gameDict["description"] = line[4]
-	return gameDict
+        gameDict = {}
+        date = parse(line[0] + " "+line [1])
+        gameDict["startDate"] = date
+        gameDict["startTime"] = line[1]
+        gameDict["teams"] = line[2]
+        gameDict["location"] = line[3]
+        gameDict["description"] = line[4]
+        return gameDict
 def makePregamePost(game):
-	print("connecting.....")
-	user_agent = "Anaheim Ducks GameDay 1.0 by /u/dahooddawg"
-	r = praw.Reddit(user_agent=user_agent)
-	o = OAuth2Util.OAuth2Util(r)
-	o.refresh(force=True)
-	r.submit('subreddit name goes here', game["startDate"].date().strftime("%B %d, %Y") +" - "+game["teams"], text=game["startTime"]+" PST - "+game["location"]+" - "+game["description"])
-	print("posted for "+game["teams"])
+        print("attempting post.....")
 
-
+        r = praw.Reddit(user_agent=user_agent)
+        o = OAuth2Util.OAuth2Util(r)
+        o.refresh(force=True)
+        r.submit(subreddit, 'Pregame Thread: '+game["startDate"].date().strftime("%B %d, %Y") +" - "+game["teams"], text=game["startTime"]+" PST - "+game["location"]+" - "+game["description"])
+        print("posted pregame for "+game["teams"])
+def makeGamePost(game):
+        print("connecting.....")
+        r = praw.Reddit(user_agent=user_agent)
+        o = OAuth2Util.OAuth2Util(r)
+        o.refresh(force=True)
+        r.submit(subreddit, 'Game Thread: '+game["teams"], text="Keep it classy and lets go Ducks!")
+        print("posted  game thread for "+game["teams"])
 
 already_done = []
+already_done_game_thread = []
 
 while(True):
-	print("checking for games today.....")
-	file = open('ducks.csv','rb')
-	reader = csv.reader(file)
-	for line in reader:
-			#convert game data to Dict
-        	game = convertToDict(line)
-        	#check if there is a game today
-    		if(game["startDate"].date() == datetime.today().date()):
-    			postTime = game["startDate"] + timedelta(hours=-12)
-    			#check if it is 12 hours before game time
-    			if(postTime < datetime.today() and game["startDate"].date() not in already_done):
-    				#make pregram thread post
-    				try:
-    					makePregamePost(game)
-    				except: 
-    					print("Connection error....will retry in 300 seconds")
-    					time.sleep(300)
+        currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print("checking for games today....." + currentTime)
+        file = open('ducks.csv','rb')
+        reader = csv.reader(file)
 
-    				#make that this game was posted so we do not duplicate any work
-    				already_done.append(game["startDate"].date())
-    				break
-	file.close()
-	time.sleep(1800)
-    #sleep for 30 mins
-    
+        for line in reader:
+                game = convertToDict(line)
+                print("checking csv:")
+                if(game["startDate"].date() == datetime.today().date()):
+                        postTime = game["startDate"] + timedelta(hours=-12)
+                        gameThreadTime = game["startDate"] + timedelta(hours=-.5)
+                        print("PostTime :")
+                        print(postTime)
+                        print("Game Thread Time :")
+                        print(gameThreadTime)
+                        if(postTime < datetime.today() and game["startDate"].date() not in already_done):
+                                print("connecting.....")
+                                try:
+                                        makePregamePost(game)
+                                except:
+                                        print("Connection error....will retry in 300 seconds")
+                                        time.sleep(300)
+                                already_done.append(game["startDate"].date())
 
 
+                        if(gameThreadTime < datetime.today() and game["startDate"].date() not in already_done_game_thread):
+                                #make game thread post
+                                try:
+                                        makeGamePost(game)
+                                except:
+                                        print("Connection error....will retry in 300 seconds")
+                                        time.sleep(300)
 
+                                        #make that this game was posted so we do not duplicate any work
+                                already_done_game_thread.append(game["startDate"].date())
+
+
+        print('Sleeping for 10 minutes......zzzzzzzzzz........zzzzzzzz.......')
+        file.close()
+        time.sleep(600)
