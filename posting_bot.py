f5910193b2a88d02789a397c000543b7e9f0f6c9
martygriffin <marty.griffin@me.com>
2017-02-02 20:46:03 -0800

    Merged changes into posting_bot.py

--- posting_bot.py
+++ posting_bot.py
@@ -10,8 +10,15 @@ import sys
 import gc
 import os
 
-user_agent = "Anaheim Ducks GameDay 1.0 by /u/dahooddawg"
-subreddit = "insert subreddit name here"
+import urllib, json
+
+
+user_agent = "Anaheim Ducks GameDay 1.1 by /u/dahooddawg"
+client="########"
+secret="########"
+username="########"
+password="########"
+ducksSubreddit='anaheimducks'
 
 
 def convertToDict(line):
@@ -25,22 +32,45 @@ def convertToDict(line):
         return gameDict
 def makePregamePost(game):
         print("attempting post.....")
-
-        r = praw.Reddit(user_agent=user_agent)
-        o = OAuth2Util.OAuth2Util(r)
-        o.refresh(force=True)
-        r.submit(subreddit, 'Pregame Thread: '+game["startDate"].date().strftime("%B %d, %Y") +" - "+game["teams"], text=game["startTime"]+" PST - "+game["location"]+" - "+game["description"])
+        r = praw.Reddit(user_agent=user_agent,password=password,username=username,client_id=client,client_secret=secret)
+        subreddit = r.subreddit(ducksSubreddit)
+        subreddit.submit('Pregame Thread: '+game["startDate"].date().strftime("%B %d, %Y") +" - "+game["teams"], selftext=game["startTime"]+" PST - "+game["location"]+" - "+game["description"])
         print("posted pregame for "+game["teams"])
-def makeGamePost(game):
-        print("connecting.....")
-        r = praw.Reddit(user_agent=user_agent)
-        o = OAuth2Util.OAuth2Util(r)
-        o.refresh(force=True)
-        r.submit(subreddit, 'Game Thread: '+game["teams"], text="Keep it classy and lets go Ducks!")
-        print("posted  game thread for "+game["teams"])
+
+def makePostGamePost(game):
+        print("attempting post.....")
+        r = praw.Reddit(user_agent=user_agent,password=password,username=username,client_id=client,client_secret=secret)
+        subreddit = r.subreddit(ducksSubreddit)
+        subreddit.submit('Post Game Thread: '+game["startDate"].date().strftime("%B %d, %Y") +" - "+game["teams"])
+        print("posted post game for "+game["teams"])
+
+def checkForPostGame(month, date, year):
+
+        url = "https://statsapi.web.nhl.com/api/v1/schedule?startDate="+str(year)+"-"+str(month)+"-"+str(date)+"&endDate="+str(year)+"-"+str(month)+"-"+str(date)+"&expand=schedule.teams,schedule.linescore&leaderCategories=&leaderGameTypes=R&site=en_nhl&teamId=&gameType=&timecode="
+     
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+
+        for day in data["dates"]:
+                for game in day["games"]:
+                        if("final" in game["status"]["detailedState"].lower()):
+                                if "ducks" in game["teams"]["away"]["team"]["teamName"].lower():
+                                        print("Ducks away game final")
+                                        return True
+                                if "ducks" in game["teams"]["home"]["team"]["teamName"].lower():
+                                        print("Ducks home game final")
+                                        return True
+
+                    
+
+        print("No Ducks game or Ducks game in progress")                                 
+        return False     
 
 already_done = []
 already_done_game_thread = []
+already_done_post_game_thread = []
+
+sleepTime = 1200
 
 while(True):
         currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -50,10 +80,9 @@ while(True):
 
         for line in reader:
                 game = convertToDict(line)
-                print("checking csv:")
                 if(game["startDate"].date() == datetime.today().date()):
                         postTime = game["startDate"] + timedelta(hours=-12)
-                        gameThreadTime = game["startDate"] + timedelta(hours=-.5)
+                        gameThreadTime = game["startDate"] + timedelta(hours=-1)
                         print("PostTime :")
                         print(postTime)
                         print("Game Thread Time :")
@@ -62,24 +91,27 @@ while(True):
                                 print("connecting.....")
                                 try:
                                         makePregamePost(game)
+                                        already_done.append(game["startDate"].date())
                                 except:
                                         print("Connection error....will retry in 300 seconds")
-                                        time.sleep(300)
-                                already_done.append(game["startDate"].date())
+                                        time.sleep(600)
+                        elif (game["startDate"].date() in already_done and game["startDate"].date() not in already_done_post_game_thread):
+                                print("attempting to make postgame postand speed up score check.....")
 
-
-                        if(gameThreadTime < datetime.today() and game["startDate"].date() not in already_done_game_thread):
-                                #make game thread post
+                                sleepTime  = 60
                                 try:
-                                        makeGamePost(game)
+                                        if(checkForPostGame(datetime.today().month,datetime.today().day, datetime.today().year)):
+                                                makePostGame(game)
+                                                already_done_post_game_thread.append(game["startDate"].date())
+                                                sleepTime = 1200
                                 except:
                                         print("Connection error....will retry in 300 seconds")
-                                        time.sleep(300)
-
-                                        #make that this game was posted so we do not duplicate any work
-                                already_done_game_thread.append(game["startDate"].date())
+                                        sleepTime = 1200
+                                        time.sleep(600)
 
 
-        print('Sleeping for 10 minutes......zzzzzzzzzz........zzzzzzzz.......')
+
+
+        print('Sleeping for'+str(sleepTime)+' seconds......zzzzzzzzzz........zzzzzzzz.......')
         file.close()
-        time.sleep(600)
+        time.sleep(sleepTime)
